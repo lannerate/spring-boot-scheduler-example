b4386b6cd4bbb898c0ff4938efe5d6389501f989
hui <zhanghuizaizhe@hotmail.com>
2019-01-06 12:02:27 +0800

    enhance async schedule task

--- src/main/java/com/example/schedulerdemo/ScheduledTasks.java
+++ src/main/java/com/example/schedulerdemo/ScheduledTasks.java
@@ -2,6 +2,7 @@ package com.example.schedulerdemo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -19,28 +20,40 @@ public class ScheduledTasks {
 
     private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
-    @Scheduled(fixedRate = 2000)
+    @Scheduled(fixedDelay = 1000)
+    @Async
     public void scheduleTaskWithFixedRate() {
-        logger.info("Fixed Rate Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()) );
+        for(int i=0; i< 10; i ++){
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                logger.error("Ran into an error {}", e);
+                throw new IllegalStateException(e);
+            }
+        }
+        logger.info("Receive Message task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()) );
     }
 
-    @Scheduled(fixedDelay = 2000)
+    @Scheduled(fixedDelay = 1000)
+    @Async
     public void scheduleTaskWithFixedDelay() {
-        logger.info("Fixed Delay Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
         try {
-            TimeUnit.SECONDS.sleep(5);
+            for(int i=0; i< 10; i ++) {
+                TimeUnit.SECONDS.sleep(5);
+            }
         } catch (InterruptedException ex) {
             logger.error("Ran into an error {}", ex);
             throw new IllegalStateException(ex);
         }
+        logger.info("Processed Message Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
     }
 
-    @Scheduled(fixedRate = 2000, initialDelay = 5000)
+//    @Scheduled(fixedRate = 2000, initialDelay = 5000)
     public void scheduleTaskWithInitialDelay() {
         logger.info("Fixed Rate Task with Initial Delay :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
     }
 
-    @Scheduled(cron = "0 * * * * ?")
+//    @Scheduled(cron = "0 * * * * ?")
     public void scheduleTaskWithCronExpression() {
         logger.info("Cron Task :: Execution Time - {}", dateTimeFormatter.format(LocalDateTime.now()));
     }
--- src/main/java/com/example/schedulerdemo/config/SchedulerConfig.java
+++ src/main/java/com/example/schedulerdemo/config/SchedulerConfig.java
@@ -1,24 +1,47 @@
 package com.example.schedulerdemo.config;
 
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.Executor;
 
 /**
  * Created by rajeevkumarsingh on 02/08/17.
  */
 @Configuration
-public class SchedulerConfig implements SchedulingConfigurer {
-    private final int POOL_SIZE = 10;
+@EnableAsync
+public class SchedulerConfig implements SchedulingConfigurer,
+        AsyncConfigurer {
+    private final int POOL_SIZE = Runtime.getRuntime().availableProcessors();
 
     @Override
     public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
         ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
         threadPoolTaskScheduler.setPoolSize(POOL_SIZE);
-        threadPoolTaskScheduler.setThreadNamePrefix("my-scheduled-task-pool-");
+        threadPoolTaskScheduler.setThreadNamePrefix("config-scheduled-task-pool-");
         threadPoolTaskScheduler.initialize();
 
         scheduledTaskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
+    }
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(POOL_SIZE);
+        executor.setThreadNamePrefix("async-scheduled-task-pool-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return new CustomAsyncExceptionHandler();
     }
 }
